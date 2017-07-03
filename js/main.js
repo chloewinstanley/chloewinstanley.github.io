3093b47b9cd6985a5f6d6a02ed2ac95b42ff6dfb
chloewinstanley <chloelouisewinstanley@gmail.com>
2017-07-03 23:49:11 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	$('#splash-name').delay(100).fadeIn(50,"linear");
+	$('#dot-1').delay(1000).fadeIn(250,"linear");
+	$('#dot-2').delay(1250).fadeIn(250,"linear");
+	$('#dot-3').delay(1750).fadeIn(250,"linear");
+	$('#dot-4').delay(2250).fadeIn(250,"linear");
+	$('#splash').delay(2700).fadeOut(300,"linear");
+
+});
