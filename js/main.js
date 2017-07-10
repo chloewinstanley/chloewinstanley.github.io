9592fee41ea2d68964a0e4ae5d170ecfe11ccfac
chloewinstanley <chloelouisewinstanley@gmail.com>
2017-07-10 18:04:02 +0100

    Crafted intro animation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
 $(document).ready(function(){
 
 	$('#splash-name').delay(2000).fadeIn(500,"linear");
-	$('#dot-1').delay(0).fadeIn(500,"linear");
-	$('#dot-2').delay(1500).fadeIn(500,"linear");
-	$('#dot-3').delay(500).fadeIn(500,"linear");
-	$('#dot-4').delay(1000).fadeIn(500,"linear");
+	
+	$('#dot-1').addClass("animated-dot");
+	setTimeout(function(){
+	
+	$('#dot-2').addClass("animated-dot");
+	}, 1200);
+
+	setTimeout(function(){
+	
+	$('#dot-3').addClass("animated-dot");
+	}, 500);
+
+	setTimeout(function(){
+	
+	$('#dot-4').addClass("animated-dot");
+	}, 1700);
+
+	setTimeout(function(){
+	
+	$('#splash-name').addClass("animated-name");
+	}, 2300);
+	
 	$('#splash').delay(5000).fadeOut(500,"linear");
+
+
+	
 
 	$('.vegan-burger').on('click', function(){
 
